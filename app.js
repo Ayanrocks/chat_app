fc3be3c93aef9ef0b23c667a8fa795aca90e4794
Ayan banerjee <ayanb1999@gmail.com>
2018-09-01 11:09:09 +0530

    RC1

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ app.get('/', (req,res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('User connected')
-})
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);    });
+});
 
 
 PORT = process.env.PORT | 3000
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log('Server Starterd');
-})
+});
